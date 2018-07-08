52d01bcb0246ad33d8b50cd650b7aa292d60df67
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-07-08 23:57:14 +1200

    Add left or right

--- js/functional/lesson2Either.js
+++ js/functional/lesson2Either.js
@@ -1,11 +1,29 @@
-const Left = x => (
-    {
-        map: (f) => Left(f(x)),
-        fold: (f) => f(x),
-        inspect: () => `Left(${x})`
-    }
+const Right = x => (
+  {
+    map: (f) => Right(f(x)),
+    fold: (f) => f(x),
+    inspect: () => `Right(${x})`
+  }
 )
 
-const Either = Left || Right
+const Left = x => (
+  {
+    map: (f) => Left(x),
+    fold: (f) => f(x),
+    inspect: () => `Left(${x})`
+  }
+)
 
-console.log(Left('hey').inspect())
+const Either = Right || Left
+
+const result = Right(2)
+  .map(x => x + 10)
+  .map(x => x / 2)
+
+console.log('Result: ', result)
+
+const result2 = Left(2)
+  .map(x => x + 10)
+  .map(x => x / 2)
+
+console.log('Result: ', result2)
