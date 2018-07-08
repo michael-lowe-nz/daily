be4afab903e66e359e8334c9a4a98807f35a8218
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-07-09 00:01:12 +1200

    Add error check functionally

--- js/functional/lesson2Either.js
+++ js/functional/lesson2Either.js
@@ -1,7 +1,7 @@
 const Right = x => (
   {
-    map: (f) => Right(f(x)),
-    fold: (f) => f(x),
+    map: f => Right(f(x)),
+    fold: (f, g) => g(x),
     inspect: () => `Right(${x})`
   }
 )
@@ -9,7 +9,7 @@ const Right = x => (
 const Left = x => (
   {
     map: (f) => Left(x),
-    fold: (f) => f(x),
+    fold: (f, g) => f(x),
     inspect: () => `Left(${x})`
   }
 )
@@ -19,11 +19,13 @@ const Either = Right || Left
 const result = Right(2)
   .map(x => x + 10)
   .map(x => x / 2)
+  .fold(x => `${x} bottles`, e => 'error')
 
 console.log('Result: ', result)
 
 const result2 = Left(2)
   .map(x => x + 10)
   .map(x => x / 2)
-
+  .fold(x => `${x} bottles`, e => 'error')
+  
 console.log('Result: ', result2)
