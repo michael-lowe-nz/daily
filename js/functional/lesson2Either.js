4fa24b037dc83c840af2c577d3e76594c684904d
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-07-09 00:12:10 +1200

    More error handling

--- js/functional/lesson2Either.js
+++ js/functional/lesson2Either.js
@@ -27,5 +27,24 @@ const result2 = Left(2)
   .map(x => x + 10)
   .map(x => x / 2)
   .fold(x => `${x} bottles`, e => 'error')
-  
+
 console.log('Result: ', result2)
+
+const findColor = color => {
+  const colors = {
+    red: '#ff9122',
+    blue: '#5e5e5e',
+    orange: '#ff9100'
+  }
+  const found = colors[color]
+  return found ? Right(found) : Left(found)
+}
+
+const result3 = findColor('blahblah')
+  .map(str => str.slice(1))
+  .fold(
+    e => 'No Color!',
+    color => color.toUpperCase()
+  )
+
+console.log('Color:', result3)
